e1b8c70bfa4165a6155f3dd60d072d72665adddf
Daniel <daniel.pitthansilveira@gmail.com>
2018-07-23 11:51:38 -0300

    Readme criado

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# WebService
-Pequeno Web Service em JavaScript e Ajax, com um callback.
+# WebService.js
+Pequeno WebService em JavaScript e Ajax, com um callback.
+
+## Objetivo 
+Esse pequeno WebService tem por objetivo facilitar a execução assincrona, de requests ao servidor.
+através de uma requisição POST.
+
+## Dependências
+* Jquery
+* JavaScript
+* HTML
+
+É meio óbvio, mas é bom mencionar, sabe como é...
+
+### Exemplo de utilização na forma que está.
+Carregue em seu HTML o script do WebService, de preferência ao final do <body>. 
+O Jquery deve ser carregado primeiro, para que tudo funcione corretamente.
+
+Exemplo:
+
+```html
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title></title>  
+        <script src="~/Scripts/jquery-3.3.1.min.js"></script>   
+    </head>
+
+    <body>
+        <main></main>
+        <script src="~/Scripts/WebService.js"></script>   
+    </body>
+
+</html>
+
+``` 
+
+Faça a chamada do WebService em seu script personalizado, conforme o exemplo abaixo:
+
+```javascript
+//Crie um objeto qualquer, por exemplo esse abaixo:
+var objetoQualquer = {
+        id:1,
+        descricao:"Uma descrição qualquer,
+        valor: 1000
+    };
+
+//Indique a Url de Destino 
+var urlDestino ="127.0.0.1:8080/RequestQualQuerCoisa/";
+
+//Informe a função de CallBack
+var callBack = function(response){
+
+    console.log(response.menssage);
+
+    if(response.success){
+        console.log("Processamento no Servidor OK");
+    }else{
+        console.log("Processamento no Servidor Não OK");
+    }
+}
+
+    
+    //Chama o webservice passando o callback 
+    //Faça a chamada do WebService 
+    WebService.Init(obj, urlDestino, callBack);
+```
+
+Variáveis que são tratadas de forma automáticas, alem do callback
+* success - Obrigatório. O callback só é executado se essa propriedade existir na sua resposta
+* menssage - Opcional. Quando informado, replica a mensagem no console.log, alêm de ser utilizável no callback.
---
+++ WebService.js
@@ -0,0 +1,93 @@
+﻿// ***********************************************************************
+// Assembly         : WebService.js
+// Author           : Daniel Pitthan Silveira
+// Created          : 06-14-2018
+//
+// Last Modified By : Daniel
+// Last Modified On : 06-15-2018
+// ***********************************************************************
+// <copyright file="WebService.js" company="DPSYS">
+//     Copyright ©  2018
+// </copyright>
+// <summary> WebService de envio de dados ao servidor 
+// Ao fim do processamento é executado o callback, caso ele exita
+// </summary >
+// ***********************************************************************
+
+var WebService = (
+    function () {
+
+
+        //Chamada do Ajax
+        //Faz o envio da informação ao Server e trata o retorno fazendo o call da promisse
+        var SendData = (obj, toUrl) => {
+            return (new Promise((resolve, reject) => {
+
+                $.ajax({
+                    url: toUrl,
+                    data: obj,
+                    dataType: "json",
+                    method: "POST",
+                    error: function (response) {
+                        console.log("Error on server. Epic Fail! | WebService.js");                        
+                        reject();
+                    },
+                    success: function (response) {
+                        if (typeof (response) == "undefined") {
+                            reject();
+                        }
+
+                        console.log("Success on request response to server! | WebService.js");
+                        if (response.success) {                            
+                            resolve(response);
+                        } else {                            
+                            resolve(response);
+                        }
+                    }
+
+                });
+
+            })
+            );
+        }
+
+        //Faz o envio para o servidor, tratando o retorno pelo callback, caso exista
+        var Send = (obj, toUrl, callBack) => {
+            SendData(obj, toUrl).then((response) => {     
+
+                if (typeof (response.success) == "undefined") {
+                    return console.log("Response object is missim the property [bool:success]");
+                }
+
+                if (response.success) {
+                    if (typeof (response.menssage) != "undefined"){
+                        console.log(response.menssage);
+                    }
+
+                    if (typeof (callBack != "undefined")) {
+                        callBack(response);
+                    }
+
+                    
+                } else {
+                    if (typeof (response.menssage) != "undefined") {
+                        console.log(response.menssage);
+                    }
+                    if (typeof (callBack != "undefined")) {
+                        callBack(response);
+                    }
+                }
+            }).catch((e) => {
+                console.log("Invalid return from server, or the server is missing, way, gone, caputs, GAME OVER!!!!!.");
+            });            
+        }
+
+        //***********************************
+        //Métodos públicos
+        //***********************************
+        return ({
+            Init: Send            
+        });
+    }
+)();
+
